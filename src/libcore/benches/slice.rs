d0f12ffa8bd593d114054c482c60945d85f9047e
Alkis Evlogimenos <alkis@evlogimenos.com>
2017-10-17 21:09:18 +0200

    Extend the benchmarks such that we test both hits and misses (they make no difference). Also use arrays such that we are in L1, L2 and L3 caches of the most common CPUs found in the wild. Results:
    before:
    
    running 3 tests
    test slice::binary_search_hit_l1                           ... bench:          48 ns/iter (+/- 2)
    test slice::binary_search_hit_l2                           ... bench:          63 ns/iter (+/- 3)
    test slice::binary_search_hit_l3                           ... bench:         143 ns/iter (+/- 7)
    
    after:
    
    running 3 tests
    test slice::binary_search_hit_l1                           ... bench:          17 ns/iter (+/- 0)
    test slice::binary_search_hit_l2                           ... bench:          24 ns/iter (+/- 0)
    test slice::binary_search_hit_l3                           ... bench:         115 ns/iter (+/- 15)

--- src/libcore/benches/slice.rs
+++ src/libcore/benches/slice.rs
@@ -11,13 +11,42 @@
 use test::black_box;
 use test::Bencher;
 
-#[bench]
-fn binary_search(b: &mut Bencher) {
-    let v = (0..999).collect::<Vec<_>>();
-    let mut i = 0usize;
+enum Cache {
+    L1,
+    L2,
+    L3,
+}
+
+fn binary_search(b: &mut Bencher, cache: Cache) {
+    let size = match cache {
+        Cache::L1 => 1000, // 8kb
+        Cache::L2 => 10_000,  // 80kb
+        Cache::L3 => 1_000_000,  // 8Mb
+    };
+    // Only store even numbers.
+    let v = (0..size).map(|i| i * 2).collect::<Vec<_>>();
+    let mut r = 0usize;
+    let max = size * 2;
     b.iter(move || {
         // LCG constants from https://en.wikipedia.org/wiki/Numerical_Recipes.
-        i = i.wrapping_mul(1664525).wrapping_add(1013904223);
-        black_box(v.binary_search(&(i % 999)).unwrap());
+        r = r.wrapping_mul(1664525).wrapping_add(1013904223);
+        // Lookup the whole range to get 50% hits and 50% misses.
+        let i = r % max;
+        black_box(v.binary_search(&i).is_ok());
     })
+}
+
+#[bench]
+fn binary_search_hit_l1(b: &mut Bencher) {
+    binary_search(b, Cache::L1);
+}
+
+#[bench]
+fn binary_search_hit_l2(b: &mut Bencher) {
+    binary_search(b, Cache::L2);
+}
+
+#[bench]
+fn binary_search_hit_l3(b: &mut Bencher) {
+    binary_search(b, Cache::L3);
 }
